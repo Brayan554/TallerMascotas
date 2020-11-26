a4ee9ffe4d1bc80d9a86716cae11af4458726c95
brayan <brayanzanabria0@gmail.com>
2020-11-26 17:10:52 +0100

    Crud Completa

--- backend/routes/mascota.js
+++ backend/routes/mascota.js
@@ -29,6 +29,54 @@ router.post("/", auth, async (req , res)=>{
     res.status(200).send(result);
 });
 
-// export
+router.get("/Listar", auth , async(req, res)=>{
 
+        const usuario = await Usuario.findById(req.usuario._id);
+        if(!usuario) return res.status(400).send("El Usuario No exite En La Bd");
+
+        const mascota = await Mascota.find({idUsuario : req.usuario._id});
+        res.send(mascota);
+});
+
+
+//Editar Listar SolaMente Una Mascota Edit
+router.put("/", auth , async(req, res)=>{
+    const usuario = await Usuario.findById(req.usuario._id);
+    if(!usuario) return res.status(400).send("El Usuarion No Existe en la bd");
+    //si el usuario Existe 
+
+    const mascota = await Mascota.findByIdAndUpdate(
+        req.body._id,
+        {
+            idUsuario: usuario._id,
+            nombre: req.body.nombre,
+            tipo: req.body.tipo,
+            descripcion: req.body.descripcion,
+        },
+        {   
+            //para que me refresque 
+            new:true,
+        }
+    );
+    //Validar Si No Existe la mascota
+    if(!mascota) return res.status(400).send("El Usuario No Tiene Mascotas");
+     
+    //si exiten tareas las devolvemos   
+    res.status(200).send(mascota);
+    
+});
+
+//Metodo Para eliminar Mascota 
+router.delete("/:_id", auth , async(req , res)=>{
+  const usuario = await Usuario.findById(req.usuario._id);
+  if(!usuario) return res.status(400).send("El Usuario No Existe En la Base De Datos");
+  //si Existe
+
+  const mascota = await Mascota.findByIdAndDelete(req.params._id);
+  if(!mascota) return res.status(400).send("No Tiene Mascotas Para Eliminar");
+  res.status(200).send({message:"Mascota Eliminada Correctamente"});
+});
+
+    
+// export
 module.exports = router;
--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -1,6 +1,8 @@
 //Nuestros modulos Internos
 
 const express = require("express");
+const { restart } = require("nodemon");
+const auth = require("../middleware/auth");
 const router  = express.Router();
 
 //Modulos Propios Creados 
@@ -29,6 +31,42 @@ router.post("/", async(req , res) => {
     const jwToken    = usuario.generateJWT();
     res.status(200).send({jwToken});
 });
+
+router.get("/listar", auth, async(req , res)=>{
+    const usuario = await Usuario.findById(req.usuario._id);
+    if(!usuario) return res.status(400).send("El Usuario No Se Encotro en la Base de Datos");
+    res.send(usuario);
+});
+
+router.put("/", auth, async (req, res)=>{
+    let usuario = await Usuario.findById(req.body._id);
+    if(!usuario) return res.status(400).send("El Usuario No se encuentra en la base de datos");
+
+    usuario = await Usuario.findByIdAndUpdate(
+        req.body._id,
+        {
+            idUsuario: usuario._id,
+            nombre: req.body.nombre,
+            correo : req.body.correo,
+            contrasena: req.body.contrasena, 
+        },
+        {
+            new:true,
+        }
+    );
+    res.status(200).send(usuario);
+});
+
+router.delete("/:_id", auth , async(req , res)=>{
+    let usuario = await Usuario.findById(req.usuario._id);
+    if(!usuario) return res.status(400).send("El Usuario No Existe En la Base De Datos");
+    //si Existe
+    
+    usuario = await Usuario.findByIdAndDelete(req.params._id);
+    res.status(200).send({message:"Eliminado Con Exito"});
+    
+  });
+  
 
 // Exports
 
